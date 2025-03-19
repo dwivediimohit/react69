755144de3bb85ff121095abd6c26bc283fe0a8bd
unknown <mail2mohitdwivedi@gmail.com>
2025-03-19 11:32:37 +0530

    MyReact1

---
+++ src/About.js
@@ -0,0 +1,19 @@
+function Myabout()
+{
+    return <h1>This is y first Node Program</h1>
+}
+export default Myabout; //Default Export
+
+
+export function Mycontainer()//Export by name
+{
+    return <h1>This is Mohit Dwivedi</h1>
+}
+
+export function Mycss()
+{
+    return <div className="page">
+        <h1>Hello Mohit</h1>
+        <section>ok</section>
+         </div>
+}
